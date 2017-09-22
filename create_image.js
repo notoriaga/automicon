a267b09d481e90cd928cf1b7ac603d23a7ca415e
Steven Meyer <steven@stdlib.com>
2017-09-22 15:31:14 -0700

    refactored main into separate functions

--- create_image.js
+++ create_image.js
@@ -1,64 +1,81 @@
-const gameOfLife = require('./gameOfLife');
-const PNG = require('pngjs').PNG;
 const fs = require('fs');
-const crypto = require('crypto');
+
 const Jimp = require('jimp');
+const PNG = require('pngjs').PNG;
 
-let testString = 'apples'
-let hash = crypto.createHash('md5').update(testString).digest("hex");
+const gameOfLife = require('./gameOfLife');
 
-let png = new PNG({
-  width: 128,
-  height: 128
-});
+let NUM_BLOCKS = 8;
+let BLOCK_SIZE = 16; // pixels
 
-let numBlocks = 8;
-let blockSize = 16; // pixels
-
-let data = gameOfLife.run(numBlocks, 100)
-
-let currentColorY = -1;
-let currentColorX = -1;
-
-for (let y = 0; y < png.height; y++) {
-
-  if (y % blockSize === 0) {
-    currentColorY++;
-  }
-
-  for (let x = 0; x < png.width; x++) {
-
-      if (x % blockSize === 0) {
-        currentColorX++;
-      }
-
-      if (currentColorX === numBlocks) {
-        currentColorX = 0;
-      }
-
-      let idx = (png.width * y + x) << 2;
-
-      png.data[idx] = data[currentColorY][currentColorX];
-      png.data[idx+1] = data[currentColorY][currentColorX];
-      png.data[idx+2] = data[currentColorY][currentColorX];
-      png.data[idx+3] = 155;
-
-  }
-
-}
-
-png.pack().pipe(fs.createWriteStream('temp.png'));
-
-Jimp.read('temp.png', function (err, quarter) {
+const md5 = (str) => {
   
-  let half = new Jimp(128, 256);
-  let whole = new Jimp(256, 256);
+  const crypto = require('crypto');
+  return crypto.createHash('md5').update(str).digest("hex");
 
-  half.blit(quarter, 0 ,0)
-      .blit(quarter.flip(false, true), 0, 128)
+};
+
+const pngFromArray = (arr) => {
+
+  let png = new PNG({
+    width: 128,
+    height: 128
+  });
+
+  let currentColorY = -1;
+  let currentColorX = -1;
+  
+  for (let y = 0; y < png.height; y++) {
+  
+    if (y % BLOCK_SIZE === 0) {
+      currentColorY++;
+    }
+  
+    for (let x = 0; x < png.width; x++) {
+  
+        if (x % BLOCK_SIZE === 0) {
+          currentColorX++;
+        }
+  
+        if (currentColorX === NUM_BLOCKS) {
+          currentColorX = 0;
+        }
+  
+        let idx = (png.width * y + x) << 2;
+  
+        png.data[idx] = arr[currentColorY][currentColorX];
+        png.data[idx+1] = arr[currentColorY][currentColorX];
+        png.data[idx+2] = arr[currentColorY][currentColorX];
+        png.data[idx+3] = 155;
+  
+    }
+  
+  }
+
+  return png;
+
+};
+
+const generateImage = (png, size = 256) => {
+
+  png.pack().pipe(fs.createWriteStream('temp.png'));
+  
+  Jimp.read('temp.png', function (err, quarter) {
     
-  whole.blit(half, 0, 0)
-       .blit(half.flip(true, false), 128, 0)
-       .write('test.png')
+    let half = new Jimp(128, 256);
+    let whole = new Jimp(256, 256);
+  
+    half.blit(quarter, 0 ,0)
+        .blit(quarter.flip(false, true), 0, 128)
+      
+    whole.blit(half, 0, 0)
+         .blit(half.flip(true, false), 128, 0)
+         .write('test.png')
+  
+    fs.unlinkSync('temp.png');
+  
+  });
 
-});
+};
+
+generateImage(pngFromArray(gameOfLife.run(NUM_BLOCKS, 100)));
