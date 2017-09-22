6aae2cf7373c17fcb7a3b00dad8e5ebe22f260a2
Steven Meyer <steven@stdlib.com>
2017-09-22 15:12:51 -0700

    image now generated from mirroring 8*8 square

--- create_image.js
+++ create_image.js
@@ -2,19 +2,20 @@ const gameOfLife = require('./gameOfLife');
 const PNG = require('pngjs').PNG;
 const fs = require('fs');
 const crypto = require('crypto');
+const Jimp = require('jimp');
 
 let testString = 'apples'
 let hash = crypto.createHash('md5').update(testString).digest("hex");
 
 let png = new PNG({
-  width: 128,//256,
-  height: 128//256,
-  //filterType: -1
+  width: 128,
+  height: 128
 });
 
-let gameOfLifeSize = 8
-let blockSize = 16;
-let data = gameOfLife.run(gameOfLifeSize, 50)
+let numBlocks = 8;
+let blockSize = 16; // pixels
+
+let data = gameOfLife.run(numBlocks, 100)
 
 let currentColorY = -1;
 let currentColorX = -1;
@@ -31,7 +32,7 @@ for (let y = 0; y < png.height; y++) {
         currentColorX++;
       }
 
-      if (currentColorX === gameOfLifeSize) {
+      if (currentColorX === numBlocks) {
         currentColorX = 0;
       }
 
@@ -46,4 +47,18 @@ for (let y = 0; y < png.height; y++) {
 
 }
 
-png.pack().pipe(fs.createWriteStream('newOut.png'));
+png.pack().pipe(fs.createWriteStream('temp.png'));
+
+Jimp.read('temp.png', function (err, quarter) {
+  
+  let half = new Jimp(128, 256);
+  let whole = new Jimp(256, 256);
+
+  half.blit(quarter, 0 ,0)
+      .blit(quarter.flip(false, true), 0, 128)
+    
+  whole.blit(half, 0, 0)
+       .blit(half.flip(true, false), 128, 0)
+       .write('test.png')
+
+});
